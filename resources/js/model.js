2fdeac31ca500f542f54c1e60b814c79ae6af764
retaLazyCodes <leo1632@hotmail.com>
2021-04-04 05:55:37 -0300

    implemented use case: store transaction in localstorage

---
+++ resources/js/model.js
@@ -0,0 +1,46 @@
+export default {
+    getNewTransactionId: getNewTransactionId,
+    saveTransactionObj: saveTransactionObj,
+    deleteTransactionObj: deleteTransactionObj,
+    thereIsDataStored: thereIsDataStored
+}
+
+
+function getNewTransactionId() {
+    let lastTransactionId = localStorage.getItem("lastTransactionId") || "-1"
+    let newTransactionId = JSON.parse(lastTransactionId) + 1
+    localStorage.setItem("lastTransactionId", JSON.stringify(newTransactionId))
+    return newTransactionId
+}
+
+
+function saveTransactionObj(transactionObj) {
+    let transactionArray = []
+
+    if (thereIsDataStored()) {
+        transactionArray = JSON.parse(localStorage.getItem("transactionData"))
+    }
+
+    transactionArray.push(transactionObj)
+    const transactionArrayJson = JSON.stringify(transactionArray)
+    localStorage.setItem("transactionData", transactionArrayJson)
+}
+
+
+function deleteTransactionObj(transactionId) {
+    let transactionArray = JSON.parse(localStorage.getItem("transactionData"))
+    let transactionIndexInArray = transactionArray.findIndex(
+        element => element.transactionId == transactionId
+    )
+
+    transactionArray.splice(transactionIndexInArray, 1)
+
+    const transactionArrayJson = JSON.stringify(transactionArray)
+    localStorage.setItem("transactionData", transactionArrayJson)
+}
+
+
+function thereIsDataStored() {
+    return localStorage.getItem("transactionData") != null
+}
+
