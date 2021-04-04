4618c5d644ec68ab2b9f27886f88b323bd7fd146
retaLazyCodes <leo1632@hotmail.com>
2021-04-04 01:52:24 -0300

    Modularizing code by responsibilities

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,19 +1,42 @@
+import { drawCategories, insertRowInTransactionTable } from './view'
+import { getNewTransactionId, saveTransactionObj } from './model'
+
+
 document.addEventListener('DOMContentLoaded', () => {
     drawCategories()
+    transactionArray = JSON.parse(localStorage.getItem("transactionData"))
+    transactionArray.forEach(element => insertRowInTransactionTable(element))
 })
 
-function drawCategories() {
-    let allCategories = [
-        "Comida", "Expensas", "Ocio", "Higiene", "Tecnología", "Trabajo", "Transporte"
-    ]
-    for (let index = 0; index < allCategories.length; index++) {
-        insertCategory(allCategories[index])
+
+const form = document.getElementById('transactionForm')
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const transactionFormData = new FormData(form)
+    const transactionObj = convertFormDataToTransactionObj(transactionFormData)
+
+    saveTransactionObj(transactionObj)
+    insertRowInTransactionTable(transactionObj)
+    form.reset()
+
+})
+
+
+function convertFormDataToTransactionObj(transactionFormData) {
+    const transactionType = transactionFormData.get('transactionType')
+    const transactionDescription = transactionFormData.get('transactionDescription')
+    const transactionAmount = transactionFormData.get('transactionAmount')
+    const transactionCategory = transactionFormData.get('transactionCategory')
+    const transactionId = getNewTransactionId()
+    return {
+        "transactionType": transactionType,
+        "transactionDescription": transactionDescription,
+        "transactionAmount": transactionAmount,
+        "transactionCategory": transactionCategory,
+        "transactionId": transactionId
     }
 }
 
-function insertCategory(categoryName) {
-    const selectElement = document.getElementById("transactionCategory")
-    const htmlToInsert = `<option> ${categoryName} </option>`
-    selectElement.insertAdjacentHTML("beforeend", htmlToInsert)
-}
 
